221f9337fe0eee951dbbc33de6c1f3b8df31dc13
adibr <adibrill15@gmail.com>
2019-10-06 17:53:56 +0300

    Initial commit

---
+++ app12/src/main/java/com/sport2gether11/Login.java
@@ -0,0 +1,21 @@
+package com.sport2gether11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClickLogin(View v) {
+        Intent i = new Intent(Login.this, MapAndMenu.class);
+        startActivity(i);
+    }
+}
---
+++ app12/src/main/java/com/sport2gether11/ProfileSettings.java
@@ -0,0 +1,14 @@
+package com.sport2gether11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
