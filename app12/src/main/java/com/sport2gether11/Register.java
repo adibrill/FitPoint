15013b1ff6c2ea45ee8c0ebcc0457ed37825be9f
sefip <sefi_piscon@windowslive.com>
2019-10-09 16:29:21 +0200

    Revert "register new user"
    This reverts commit 952936c4c9e88789fb5d716ac167c3e387d21329.

--- app12/src/main/java/com/sport2gether11/Register.java
+++ app12/src/main/java/com/sport2gether11/Register.java
@@ -1,29 +1,16 @@
 package com.sport2gether11;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.sport2gether11.Login;
-import com.sport2gether11.MapAndMenu;
-import com.sport2gether11.R;
 
 public class Register extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword, inputUsername;
-    private ProgressBar progressBar;
+    private EditText inputEmail, inputPassword, inputUserName;
 
     private String userId;
     private String email;
@@ -37,10 +24,10 @@ public class Register extends AppCompatActivity {
 
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
-        inputUsername = (EditText) findViewById(R.id.username);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        inputUserName = (EditText) findViewById(R.id.username);
 
         mAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
@@ -49,70 +36,8 @@ public class Register extends AppCompatActivity {
 
         // if user logged in, go to sign-in screen
         if (mAuth.getCurrentUser() != null) {
-            startActivity(new Intent(this, Login.class));
+            startActivity(new Intent(this, MapAndMenu.class));
             finish();
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        progressBar.setVisibility(View.GONE);
-    }
-
-    public void onRegisterClicked(View view) {
-        String emailInput = inputEmail.getText().toString().trim();
-        String password = inputPassword.getText().toString().trim();
-        final String username = inputUsername.getText().toString().trim();
-
-        if (TextUtils.isEmpty(username)) {
-            Toast.makeText(getApplicationContext(), "Enter username!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (TextUtils.isEmpty(emailInput)) {
-            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (password.length() < 6) {
-            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        progressBar.setVisibility(View.VISIBLE);
-
-        //create user
-        mAuth.createUserWithEmailAndPassword(emailInput, password)
-                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Toast.makeText(Register.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
-                        progressBar.setVisibility(View.GONE);
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(Register.this, "Authentication failed." + task.getException(),
-                                    Toast.LENGTH_LONG).show();
-                            Log.e("MyTag", task.getException().toString());
-                        } else {
-
-                            startActivity(new Intent(Register.this, Login.class));
-                            finish();
-                        }
-                    }
-                });
-    }
-
-    public void onLoginClicked(View view) {
-        startActivity(new Intent(this, Login.class));
     }
 }
