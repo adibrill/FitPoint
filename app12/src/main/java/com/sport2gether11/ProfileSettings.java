734715b3bbe252764d273ea7319374b8015836fb
sefip <sefi_piscon@windowslive.com>
2019-10-13 15:47:47 +0200

    95 - user location (bug)

--- app12/src/main/java/com/sport2gether11/ProfileSettings.java
+++ app12/src/main/java/com/sport2gether11/ProfileSettings.java
@@ -1,22 +1,56 @@
 package com.sport2gether11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class ProfileSettings extends AppCompatActivity implements LocationListener {
+
     LocationManager locationManager;
+    LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            // if both are true, we have permission
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, locationListener);
+
+            }
+        }
     }
 
     public void onClickContinue(View V){
@@ -36,9 +70,9 @@ public class ProfileSettings extends AppCompatActivity implements LocationListen
 
     public void onClickUpdateLocation(View V){
         try {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (LocationListener) this);
-            Toast.makeText(ProfileSettings.this, "", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ProfileSettings.this, "Location test", Toast.LENGTH_SHORT).show();
 
         }
         catch(SecurityException e) {
@@ -49,6 +83,7 @@ public class ProfileSettings extends AppCompatActivity implements LocationListen
 
        // locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
         Toast.makeText(ProfileSettings.this, "Current Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        Log.i("Location", location.toString());
 
     }
     @Override
