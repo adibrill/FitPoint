b1380cfb7acfbd7ff5c852d7ef2248294648d8f0
adibr <adibrill15@gmail.com>
2019-10-13 14:53:41 +0300

    workouts list

--- app12/src/main/java/com/sport2gether11/ProfileSettings.java
+++ app12/src/main/java/com/sport2gether11/ProfileSettings.java
@@ -2,13 +2,58 @@ package com.sport2gether11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class ProfileSettings extends AppCompatActivity {
-
+public class ProfileSettings extends AppCompatActivity implements LocationListener {
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
     }
+
+    public void onClickContinue(View V){
+        Intent intent = new Intent(this, MapAndMenu.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    public void onClickUpdateLocation(View V){
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (LocationListener) this);
+            Toast.makeText(ProfileSettings.this, "", Toast.LENGTH_SHORT).show();
+
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    public void onLocationChanged(Location location) {
+
+       // locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
+        Toast.makeText(ProfileSettings.this, "Current Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
 }
