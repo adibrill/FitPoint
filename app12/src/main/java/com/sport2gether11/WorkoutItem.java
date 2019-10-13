19ae234c075d6b4bca8604ed1bac72f109431d2e
adibr <adibrill15@gmail.com>
2019-10-13 12:10:06 +0300

    workouts list

---
+++ app12/src/main/java/com/sport2gether11/WorkoutItem.java
@@ -0,0 +1,42 @@
+package com.sport2gether11;
+
+import java.util.jar.Attributes;
+
+public class WorkoutItem
+{
+    private int mImageResource;
+    private String mName;
+    private String mWorkoutTime;
+
+    public WorkoutItem(int Resource,String Name1,String Workout2)
+    {
+        mImageResource = Resource;
+        mName = Name1;
+        mWorkoutTime=Workout2;
+    }
+
+
+    public int getmImageResource() {
+        return mImageResource;
+    }
+
+    public void setmImageResource(int mImageResource) {
+        this.mImageResource = mImageResource;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmWorkoutTime() {
+        return mWorkoutTime;
+    }
+
+    public void setmWorkoutTime(String mWorkoutTime) {
+        this.mWorkoutTime = mWorkoutTime;
+    }
+}
--- app12/src/main/java/com/sport2gether11/ui/notifications/NotificationsFragment.java
+++ app12/src/main/java/com/sport2gether11/ui/notifications/NotificationsFragment.java
@@ -12,10 +12,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sport2gether11.R;
+import com.sport2gether11.WorkoutItem;
+import com.sport2gether11.WorkoutsListAdapter;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -26,16 +36,30 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        ArrayList<WorkoutItem> workoutlog = new ArrayList<>();
+
+        // TODO
+        //add firebase data!!!
+
+
+        workoutlog.add(new WorkoutItem(R.drawable.ic_menu_camera,"Jenny","2019/10/15 15:00"));
+        workoutlog.add(new WorkoutItem(R.drawable.ic_menu_camera,"Andrew","2019/10/17 18:30"));
+        workoutlog.add(new WorkoutItem(R.drawable.ic_menu_camera,"Jenny","2019/10/18 20:00"));
 
 
 
+
+
+
+
+        mRecyclerView = (RecyclerView)root.findViewById(R.id.workoutsrecycler);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mAdapter = new WorkoutsListAdapter(workoutlog);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
         return root;
     }
 }
